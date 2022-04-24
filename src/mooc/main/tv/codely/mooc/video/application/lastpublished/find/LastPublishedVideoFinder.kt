99b9b69f00278e979a73e40f7f4639a620eebf52
Alejandro <alexibanovella@gmail.com>
2022-04-25 00:13:05 +0200

    Revert "Added GetLatestVideoPublished"
    This reverts commit 860df7b4

--- src/mooc/main/tv/codely/mooc/video/application/lastpublished/find/LastPublishedVideoFinder.kt
+++
@@ -1,9 +0,0 @@
-package tv.codely.mooc.video.application.lastpublished.find
-
-import tv.codely.mooc.video.domain.VideoRepository
-
-class LastPublishedVideoFinder constructor(private val videoRepository: VideoRepository) {
-
-    // how to treat exceptions here??
-    fun getLastPublishedVideo() = videoRepository.getLastPublishedVideo()
-}
--- src/mooc/main/tv/codely/mooc/video/domain/VideoRepository.kt
+++
@@ -1,8 +0,0 @@
-package tv.codely.mooc.video.domain
-
-interface VideoRepository {
-
-    fun save(video: Video)
-
-    fun getLastPublishedVideo(): Video
-}
